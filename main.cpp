106a27ff5e39520f695bedc10aaae3297e430683
Lucky Woods <lucjwood@ut.utm.edu>
2021-11-10 11:56:21 -0600

    Adding new main.cpp.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 
 int sum(int n);
+int product(int n);
+
 
 int main() {
      std::cout << "Hello World!" << std::endl;
@@ -9,7 +11,8 @@ int main() {
      std::cout << "Number please: ";
      std::cin >> n;
      std::cout << "Sum is " << sum(n) << std::endl;
-     
+     std::cout << "Product is " << product(n) << std::endl;
+
      
      return 0;
 
@@ -19,6 +22,16 @@ int sum(int n){
     int total = 0;
     for(int i = 0; i < n; i++){
         total = total + i + 1;
+    }
+
+    return total;
+
+}
+
+int product(int n){
+    int total = 1;
+    for(int i = 0; i < n; i++){
+        total = total * (i + 1);
     }
 
     return total;
